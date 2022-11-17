d1159e8e6f6b2aee3902a8627f9847b478709c3a
Maurício Casagrande <61338766+bighouse77@users.noreply.github.com>
2022-11-17 06:57:51 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,64 @@
 <template>
-  <TheHeader v-show="showHeader"/>
-
-  <div v-show="showName">
-    Nome: {{ firstName }} <br>
-    Sobernome: {{ lastName }}
+  <div v-for="(obj, index) in todos" v-bind:key="obj.id" class="todos-item">
+    {{ index }} {{ obj.title }}
   </div>
-
-  <div v-if="accessLevel == 'admin'">Admin</div>
-  <div v-else-if="accessLevel == 'marketing'">Marketing</div>
-  <div v-else>User</div>
-
-  <img alt="Vue logo" src="./assets/logo.png">
-
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    TheHeader
-  },
+  name: 'App' ,
   data() {
     return {
-      showHeader: true,
-      showName: true,
-      firstName: "Maurício",
-      lastName: "Casagrande",
-      accessLevel: "admin"
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
     }
   }
 }
 </script>
 
-
 <style>
+.todos-item {
+  background-color: blueviolet;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
