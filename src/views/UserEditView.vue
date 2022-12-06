33a4ffeaabf12e0cd46f757e31a92a53164a91ce
Maurício Casagrande <mauricio.araras@hotmail.com>
2022-12-06 10:12:53 -0300

    0612 1010

--- src/views/UserEditView.vue
+++ src/views/UserEditView.vue
@@ -3,6 +3,13 @@
     <div>
         {{$route.params.id}}
         Usuarios
+
+        <br>
+
+        <button @click="login()">
+            Login
+        </button>
+
     </div>
 
 </template>
@@ -10,7 +17,15 @@
 <script>
     export default {
         created() {
-            console.log(this.$route);
+            console.log(this.$router);
+        },
+
+        methods: {
+            login(){
+                // ajax login
+                this.$router.push({ name: 'about' });
+                console.log("clicou");
+            }
         }
     }
 </script>
